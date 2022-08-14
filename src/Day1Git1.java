822b4b1d6523ac0c0b8b608e1efe2d254e88f3d1
Ozgur Altug <ozguraltug06@gmail.com>
2022-08-14 17:09:30 +0300

    3.C

--- src/Day1Git1.java
+++ src/Day1Git1.java
@@ -1,4 +1,4 @@
-public class day1Git {
+public class Day1Git1 {
     public static void main(String[] args) {
         System.out.println("Hello Git 1.Deneme");
         System.out.println("Hello Git 2.Deneme");
