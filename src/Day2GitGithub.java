119d4306a98d5d31e8715f97658c477d67048004
Ozgur Altug <ozguraltug06@gmail.com>
2022-08-15 21:38:56 +0300

    V5

--- src/Day2GitGithub.java
+++ src/Day2GitGithub.java
@@ -9,13 +9,12 @@ public class Day2GitGithub {
  4-)git commiit -m "mesaj" --> Staging area'dan commit stora
  dosyalarımı göndermek için kullanılır
  (commit yani versiyon yani sürüm oluşturmuş olurum)
- 5-) git push --> Uzak repo'ya(yani remote - GitHub)
+ 5- git push --> Uzak repo'ya(yani remote - GitHub)
   göndermek için kullandıgımız kod
  yalnız git push komutunu diirekt kullanmak istersek 1 kez
          git remote add origin adress
          git push -u origin master
-NOT: Yukarıdaki iki komutu tek seferde kullandıktan sonra ikinci commit'lerim için sadece git push
-kullanırız
+NOT: Yukarıdaki iki komutu tek seferde kullandıktan sonra ikinci commit'lerim için sadece git puskullanırız
 
   */
    public static void main(String[] args) {
