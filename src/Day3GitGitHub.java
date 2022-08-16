f45e46b3590ebc850a4915546f4a36fc7b2e5a9b
Ozgur Altug <ozguraltug06@gmail.com>
2022-08-16 20:47:44 +0300

    C3

--- src/Day3GitGitHub.java
+++ src/Day3GitGitHub.java
@@ -2,5 +2,6 @@ public class Day3GitGitHub {
     public static void main(String[] args) {
         System.out.println("3.gun 1.deneme");
         System.out.println("3.gun 2.deneme");
+        System.out.println("3.gun branch 3.deneme");
     }
 }
